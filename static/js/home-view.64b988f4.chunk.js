10dd13c9b980a71c03cbfd352a9f6bd79b2f2f9c
Uliya Snisar <Uliya19191@gmail.com>
2021-08-09 20:27:06 +0300

    Updates

--- static/js/home-view.64b988f4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[1],{131:function(e,o,t){},137:function(e,o,t){"use strict";t.r(o);t(0),t(131);var n=t(2);o.default=function(){return Object(n.jsx)("div",{className:"HomeContainer",children:Object(n.jsx)("h1",{className:"Home-title",children:"Phonebook"})})}}}]);
-//# sourceMappingURL=home-view.64b988f4.chunk.js.map
